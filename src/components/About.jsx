64769adcf80e18a59096576779b11fb6ee786a1a
abdiel-codes <abdiel.dev@foxcoon.studio>
2025-03-17 20:41:30 -0600

    Improved about section with text and style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,20 @@ function About() {
     return (
         <div className="about-container">
             <h1 className="about-title">
-                Hi my name is Abdiel
+                Hi! My name is Abdiel Flores, a Full-Stack Developer.
             </h1>
             
             <p className="about-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minus quisquam sunt dolores animi voluptas, tempore vel adipisci non voluptatibus doloremque culpa nihil ea dicta. Delectus, nesciunt provident. Asperiores, veniam!
+                I have experience working with JavaScript, CSS, React, C# and Python, focusing on creating responsive, scalable and accesible web applications.
+            </p>
+            <p className="about-text">
+                My journey into software development started with a passion for technology and curiosity about how things work. Over time, i have worked on various projects, improving both, my front-end and back-end skills.
+            </p>
+            <p className="about-text">
+                I believe that great software development is not just about writting code but also about creating intuitive and accesible experiences. That's why I pay attention to both performance and user experience.
+            </p>
+            <p className="about-text">
+                When I'm not coding, I enjoy exploring new technologies, listening to podcasts, and improving my problem-solving skills. I'm always open to new challenges and collaborations!
             </p>
         </div>
     )
